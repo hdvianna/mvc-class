76ec7054917ed457b44319155d66e599af3d5feb
Henrique Damasceno Vianna <hdvianna@gmail.com>
2022-11-06 19:21:45 -0300

    refacto controller tests

--- test/drivers/controller.test.js
+++ test/drivers/controller.test.js
@@ -3,38 +3,50 @@ import chance from 'chance';
 
 test('Given the application is set when calling the create port then model.create, presenter.makeFromResult, and output.send are called', () => {
     
-    const expectedModelResult = chance().string();;
-    const modelCreateMock = jest.fn();
-    modelCreateMock.mockReturnValueOnce(expectedModelResult);
-    const modelMock = {
-        "create": modelCreateMock
-    };
+    const expectedModelResult = chance().string();
+    const modelMock = makeModelMock(expectedModelResult);
 
-    const expectedPresenterResult = chance().string();;
-    const presenterMakeFromResultMock = jest.fn();
-    presenterMakeFromResultMock.mockReturnValueOnce(expectedPresenterResult);
-    const presenterMock = {
-        "makeFromResult": presenterMakeFromResultMock
-    };
+    const expectedPresenterResult = chance().string();
+    const presenterMock = makePresenterMock(expectedPresenterResult);
 
     const adapterMock = {};    
-    const viewSendMock = jest.fn();    
-    const viewMock = {
-        "send": viewSendMock
-    }
+    const viewMock = makeViewMock();
     
     const driversController = driversControllerModule(modelMock, presenterMock);
     driversController.create(viewMock, adapterMock);
 
-    expect(modelCreateMock).toHaveBeenCalledTimes(1);
-    expect(modelCreateMock).toHaveReturnedWith(expectedModelResult);
+    expect(modelMock.create).toHaveBeenCalledTimes(1);
+    expect(modelMock.create).toHaveReturnedWith(expectedModelResult);
 
-    expect(presenterMakeFromResultMock).toHaveBeenCalledWith(expectedModelResult, modelMock);
-    expect(presenterMakeFromResultMock).toHaveBeenCalledTimes(1);
-    expect(presenterMakeFromResultMock).toHaveReturnedWith(expectedPresenterResult);
+    expect(presenterMock.makeFromResult).toHaveBeenCalledWith(expectedModelResult, modelMock);
+    expect(presenterMock.makeFromResult).toHaveBeenCalledTimes(1);
+    expect(presenterMock.makeFromResult).toHaveReturnedWith(expectedPresenterResult);
     
-    expect(viewSendMock).toHaveBeenCalledTimes(1);
-    expect(viewSendMock).toHaveBeenCalledWith(expectedPresenterResult);
-
+    expect(viewMock.send).toHaveBeenCalledTimes(1);
+    expect(viewMock.send).toHaveBeenCalledWith(expectedPresenterResult);
 
 });
+
+
+function makeModelMock(expectedModelResult) {    
+    const modelCreateMock = jest.fn();
+    modelCreateMock.mockReturnValueOnce(expectedModelResult);
+    return {
+        "create": modelCreateMock
+    };
+}
+
+function makePresenterMock(expectedPresenterResult) {
+    const presenterMakeFromResultMock = jest.fn();
+    presenterMakeFromResultMock.mockReturnValueOnce(expectedPresenterResult);
+    return {
+        "makeFromResult": presenterMakeFromResultMock
+    };
+}
+
+function makeViewMock() {
+    const viewSendMock = jest.fn();    
+    return {
+        "send": viewSendMock
+    }
+}
